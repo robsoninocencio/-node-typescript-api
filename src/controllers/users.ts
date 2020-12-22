946252f7e965261eeca99b8ade89a2de1eeaa09e
robsoninocencio <robson.inocencio@gmail.com>
2020-12-22 18:35:15 -0300

    [C05P02] - Módulo de Users - Mongoose hooks, validation e Type narrowing

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,22 +1,18 @@
 import { Controller, Post } from '@overnightjs/core';
-import { Request, Response } from 'express';
+import { Response, Request } from 'express';
 import { User } from '@src/models/user';
-import mongoose from 'mongoose';
+import { BaseController } from './index';
 
 @Controller('users')
-export class UsersController {
+export class UsersController extends BaseController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
     try {
       const user = new User(req.body);
-      const result = await user.save();
-      res.status(201).send(result);
+      const newUser = await user.save();
+      res.status(201).send(newUser);
     } catch (error) {
-      if (error instanceof mongoose.Error.ValidationError) {
-        res.status(422).send({ error: error.message });
-      } else {
-        res.status(500).send({ error: 'Internal Server Error' });
-      }
+      this.sendCreateUpdateErrorResponse(res, error);
     }
   }
 }
--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -2,6 +2,7 @@ import { User } from '@src/models/user';
 
 describe('Users functional tests', () => {
   beforeAll(async () => await User.deleteMany({}));
+
   describe('When creating a new user', () => {
     it('should create a user with success', async () => {
       const newUser = {
@@ -16,20 +17,35 @@ describe('Users functional tests', () => {
       expect(response.body).toEqual(expect.objectContaining(newUser));
     });
 
-    // it('should return 422 when there is a validation error', async () => {
-    //   const newUser = {
-    //     name: 'Robson Inocêncio',
-    //     email: 'robson.inocencio@gmail.com',
-    //     password: '123',
-    //   };
-    //   const response = await global.testRequest.post('/users').send(newUser);
+    it('Should return 422 when there is a validation error', async () => {
+      const newUser = {
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      const response = await global.testRequest.post('/users').send(newUser);
 
-    //   expect(response.status).toBe(422);
-    //   expect(response.body).toEqual({
-    //     error:
-    //       'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
-    //   });
-    // });
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        code: 422,
+        error: 'User validation failed: name: Path `name` is required.',
+      });
+    });
+
+    it('Should return 409 when the email already exists', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      await global.testRequest.post('/users').send(newUser);
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(409);
+      expect(response.body).toEqual({
+        code: 409,
+        error: 'User validation failed: email: already exists in the database.',
+      });
+    });
 
     // it.skip('should return 500 when there is any error other than validation error', async () => {
     //   //TODO think in a way to throw a 500
